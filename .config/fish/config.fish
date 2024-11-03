690e41a9cfe23899457de1665777a0f5b8ec4ea0
Cuong Nguyen <nguyenquocuong.it@gmail.com>
2024-11-03 14:44:09 +0700

    add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+set fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+  fenv source /etc/profile
+end
+
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+	exec startx -- -keeptty
+    end
+end
+
+eval $(ssh-agent -c) &>/dev/null
+
