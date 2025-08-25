707f93fda66ff51e4ada7ffd6a287a75e5d29eea
Cuong Nguyen <nguyenquocuong.it@gmail.com>
2025-08-25 14:53:07 +0700

    fix(neovim): format with prettier if prettier config files exists

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -15,6 +15,22 @@ return {
           javascript = { "prettier" },
           javascriptreact = { "prettier" },
         },
+        formatters = {
+          prettier = {
+            condition = function(self, ctx)
+              local root = require("conform.util").root_file({
+                ".prettierrc",
+                ".prettierrc.json",
+                ".prettierrc.js",
+                ".prettierrc.cjs",
+                "prettier.config.js",
+                "prettier.config.cjs",
+              })(self, ctx)
+
+              return root ~= nil
+            end
+          }
+        }
       })
     end
   },
