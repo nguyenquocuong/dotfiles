8dac9341c2555dad3e3df9d6cabe29df7a1ded6b
Cuong Nguyen <nguyenquocuong.it@gmail.com>
2025-09-13 09:31:24 +0700

    feat(neovim): multi cursor nvim

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -7,6 +7,7 @@ return {
 
       conform.setup({
         formatters_by_ft = {
+          json = { "jq" },
           lua = { "stylua" },
           sql = { "sleek" },
           python = { "isort", "black" },
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,42 +1,98 @@
 return {
-	-- LazyVim base (no extras)
-	{ "folke/lazy.nvim" },
-	{ "nvim-lua/plenary.nvim" },
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("which-key").setup({
-				plugins = {
-					spelling = { enabled = true }, -- shows spelling suggestions
-				},
-				win = {
-					border = "single",
-				},
-			})
-		end,
-	},
+  -- LazyVim base (no extras)
+  { "folke/lazy.nvim" },
+  { "nvim-lua/plenary.nvim" },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("which-key").setup({
+        plugins = {
+          spelling = { enabled = true }, -- shows spelling suggestions
+        },
+        win = {
+          border = "single",
+        },
+      })
+    end,
+  },
 
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	},
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
+  },
 
-	{
-		"mg979/vim-visual-multi",
-		branch = "master",
-		lazy = false,
-		init = function()
-			vim.g.VM_maps = {
-				["Find Under"] = "<C-d>",
-				["Find Subword Under"] = "<C-d>",
-				["Add Cursor Down"] = "<C-j>",
-				["Add Cursor Up"] = "<C-k>",
-			}
-		end,
-	},
+  {
+    "jake-stewart/multicursor.nvim",
+    branch = "1.0",
+    config = function()
+      local mc = require("multicursor-nvim")
+      mc.setup()
+
+      local set = vim.keymap.set
+
+      -- Add or skip cursor above/below the main cursor.
+      set({ "n", "x" }, "<C-S-up>", function() mc.lineAddCursor(-1) end)
+      set({ "n", "x" }, "<C-S-down>", function() mc.lineAddCursor(1) end)
+      set({ "n", "x" }, "<leader><up>", function() mc.lineSkipCursor(-1) end)
+      set({ "n", "x" }, "<leader><down>", function() mc.lineSkipCursor(1) end)
+
+      -- Add or skip adding a new cursor by matching word/selection
+      set({ "n", "x" }, "<leader>n", function() mc.matchAddCursor(1) end)
+      set({ "n", "x" }, "<leader>s", function() mc.matchSkipCursor(1) end)
+      set({ "n", "x" }, "<leader>N", function() mc.matchAddCursor(-1) end)
+      set({ "n", "x" }, "<leader>S", function() mc.matchSkipCursor(-1) end)
+
+      -- Add a cursor for all matches of cursor word/selection in the document.
+      set({ "n", "x" }, "<leader>A", mc.matchAllAddCursors)
+
+      -- Add or skip adding a new cursor by matching diagnostics.
+      -- set({ "n", "x" }, "]d", function() mc.diagnosticAddCursor(1) end)
+      -- set({ "n", "x" }, "[d", function() mc.diagnosticAddCursor(-1) end)
+      -- set({ "n", "x" }, "]s", function() mc.diagnosticSkipCursor(1) end)
+      -- set({ "n", "x" }, "[S", function() mc.diagnosticSkipCursor(-1) end)
+
+      -- Add and remove cursors with control + left click.
+      set("n", "<c-leftmouse>", mc.handleMouse)
+      set("n", "<c-leftdrag>", mc.handleMouseDrag)
+      set("n", "<c-leftrelease>", mc.handleMouseRelease)
+
+      -- Disable and enable cursors.
+      set({ "n", "x" }, "<c-q>", mc.toggleCursor)
+
+      -- Mappings defined in a keymap layer only apply when there are
+      -- multiple cursors. This lets you have overlapping mappings.
+      mc.addKeymapLayer(function(layerSet)
+        -- Select a different cursor as the main one.
+        layerSet({ "n", "x" }, "<left>", mc.prevCursor)
+        layerSet({ "n", "x" }, "<right>", mc.nextCursor)
+
+        -- Delete the main cursor.
+        layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+
+        -- Enable and clear cursors using escape.
+        layerSet("n", "<esc>", function()
+          if not mc.cursorsEnabled() then
+            mc.enableCursors()
+          else
+            mc.clearCursors()
+          end
+        end)
+      end)
+
+      -- Customize how cursors look.
+      local hl = vim.api.nvim_set_hl
+      hl(0, "MultiCursorCursor", { reverse = true })
+      hl(0, "MultiCursorVisual", { link = "Visual" })
+      hl(0, "MultiCursorSign", { link = "SignColumn" })
+      hl(0, "MultiCursorMatchPreview", { link = "Search" })
+      hl(0, "MultiCursorDisabledCursor", { reverse = true })
+      hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+      hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+    end
+  }
 }
